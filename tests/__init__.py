d9ec44df5e4c60d42ae94a97b9a646115b6efb98
Chris Mutel <cmutel@gmail.com>
2017-04-07 10:51:01 +0200

    Rearrange tests for Windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,22 @@
-from .backend_mock import config
 from brightway import backend_mapping
 from brightway.projects import ProjectManager
 import pytest
 import shutil
 import tempfile
+
+
+class MockBackendConfig:
+    def __init__(self):
+        self.path = None
+
+    def activate(self, dirpath):
+        self.path = dirpath
+
+    def deactivate(self):
+        self.path = None
+
+
+config = MockBackendConfig()
 
 
 @pytest.fixture
