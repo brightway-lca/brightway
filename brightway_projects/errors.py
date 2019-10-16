756de1d273bf50f044866e5f7a6bde24edbd6b0a
Chris Mutel <cmutel@gmail.com>
2019-10-16 11:24:29 +0200

    Move processing to new repo

--- brightway_projects/errors.py
+++ brightway_projects/errors.py
@@ -8,27 +8,7 @@ class MissingBackend(BrightwayError):
     pass
 
 
-class InvalidName(BrightwayError):
-    """Name fails datapackage requirements:
-
-    A short url-usable (and preferably human-readable) name of the package. This MUST be lower-case and contain only alphanumeric characters along with ".", "_" or "-" characters."""
-
-    pass
-
-
 class DuplicateDtypeLabel(BrightwayError):
     """Dtype has conflicting labels"""
-
-    pass
-
-
-class InconsistentFields(BrightwayError):
-    """Given fields not the same for each element"""
-
-    pass
-
-
-class NonUnique(BrightwayError):
-    """Nonunique elements when uniqueness is required"""
 
     pass
